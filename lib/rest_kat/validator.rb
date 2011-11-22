c2b20bb2f011cbebd9e83a7db1c495b57d544712
Brad Phelan <bradphelan@xtargets.com>
2011-11-22 17:05:20 +0100

    Fix the validator to work using an input schema

--- lib/rest_kat/validator.rb
+++ lib/rest_kat/validator.rb
@@ -3,50 +3,49 @@ require 'kwalify'
 module RestKat
   ## validator class for answers
   class Validator < Kwalify::Validator
-    ## load schema definition
-    @@schema = Kwalify::Yaml.load_file(  File.join Rails.root, "lib/rest_kit/api_schema.yml" )
-    def self.schema
-      @@schema
+
+    def schema
+      @schema
     end
 
-    def resource_schema
-      @resource_schema
-    end
+	def self.load_schema file
+		Kwalify::Yaml.load_file file
+	end
 
     def initialize type
       super (@resource_schema=type)
     end
 
-    def self.find_resource resource
-      Validator.schema["resources"].find do |r|
+    def self.find_resource resource, schema
+      schema["resources"].find do |r|
         r["name"].to_s == resource.to_s
       end
     end
 
-    def self.for_resource resource
-      type = find_resource(resource)["type"]
-      raise Exception.new("Can't find resource '#{resource}'") unless type
-      pp type
-      Validator.new type
+    def self.for_resource resource, file
+		schema = load_schema file
+		type = find_resource(resource, schema)["type"]
+		raise Exception.new("Can't find resource '#{resource}'") unless type
+		Validator.new type
     end
 
-    def self.for_type type
-      type = Validator.schema[type.to_s]
-      raise Exception.new("Can't find resource '#{resource}'") unless type
-      pp type
-      Validator.new type
-    end
+    def self.for_type type, file
+		schema = load_schema file
+		type = schema[type.to_s]
+		raise Exception.new("Can't find resource '#{resource}'") unless type
+		Validator.new type
+	end
 
-    def self.for_resource_collection resource
-      type = find_resource(resource)["type"]
-      raise Exception.new("Can't find resource '#{resource}'") unless type
-      collection_type = {
-        "type" => "seq",
-        "sequence" => [ type ]
-      }
+    def self.for_resource_collection resource, file
+		schema = load_schema file
+		type = find_resource(resource, schema)["type"]
 
-      Validator.new collection_type
-    end
+		collection_type = {
+			"type" => "seq",
+			"sequence" => [ type ]
+		}
+		Validator.new collection_type
+	end
 
   end
 
